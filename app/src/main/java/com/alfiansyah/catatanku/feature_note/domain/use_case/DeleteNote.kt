7a1b1f2cb6b0ed9eedc2fc18bcdaf4733e596f1d
achmadalfiansyah <achmadalfiansyah069@gmail.com>
2024-05-28 15:22:54 +0700

    Create Use cases

---
+++ app/src/main/java/com/alfiansyah/catatanku/feature_note/domain/use_case/DeleteNote.kt
@@ -0,0 +1,12 @@
+package com.alfiansyah.catatanku.feature_note.domain.use_case
+
+import com.alfiansyah.catatanku.feature_note.domain.model.Note
+import com.alfiansyah.catatanku.feature_note.domain.repository.NoteRepository
+
+class DeleteNote(
+    private val repository: NoteRepository
+) {
+    suspend operator fun invoke(note: Note){
+        repository.deleteNote(note)
+    }
+}
